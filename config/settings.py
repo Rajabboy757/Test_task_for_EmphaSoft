96c17196587003bb8d78496bdb8a8daf632c92b4
Rajabboy757 <rajrajrajradju@gmail.com>
2023-07-01 21:47:00 +0500

    started new app connected the postgresql and did initial migrations

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hotel',
 ]
 
 MIDDLEWARE = [
@@ -66,11 +67,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'test',
+#        'USER': '<database_username>',
+#        'PASSWORD': '<password>',
+#        'HOST': '<database_hostname_or_ip>',
+#        'PORT': '<database_port>',
+#    }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DATABASE_URL',
+                      default='postgres://postgres:1699@localhost:5432/test'),
 }
 
 # Password validation
